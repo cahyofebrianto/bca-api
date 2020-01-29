992d763c1b169bbbd3ca6f9e64c6a996def68a31
kedifeles <kedifeleskatten@protonmail.com>
2020-01-29 08:45:11 +0700

    BankingFundTransfer: use retry v3

--- bca_banking.go
+++ bca_banking.go
@@ -3,6 +3,7 @@ package bca
 import (
 	"context"
 
+	"github.com/avast/retry-go"
 	"github.com/juju/errors"
 	bcaCtx "github.com/purwaren/bca-api/context"
 )
@@ -25,7 +26,7 @@ func (b *BCA) BankingGetBalance(ctx context.Context, dtoReq BalanceInfoRequest) 
 	return dtoResp, nil
 }
 
-func (b *BCA) BankingFundTransfer(ctx context.Context, dtoReq FundTransferRequest) (*FundTransferResponse, error) {
+func (b *BCA) BankingFundTransfer(ctx context.Context, dtoReq FundTransferRequest) (dtoResp *FundTransferResponse, err error) {
 	ctx = bcaCtx.WithBCASessID(ctx, b.bcaSessID)
 
 	dtoReq.CorporateID = b.config.CorporateID
@@ -33,7 +34,15 @@ func (b *BCA) BankingFundTransfer(ctx context.Context, dtoReq FundTransferReques
 	b.log(ctx).Info("=== START BANKING FUND_TRANSFER ===")
 	b.log(ctx).Infof("REQUEST: %+v", dtoReq)
 
-	dtoResp, err := b.api.bankingPostFundTransfer(ctx, dtoReq)
+	retryOpts := b.retryOptions(ctx)
+	err = retry.Do(func() error {
+		if dtoResp, err = b.api.bankingPostFundTransfer(ctx, dtoReq); err != nil {
+			return err
+		}
+		return errorIfErrCodeESB14009(dtoResp.Error)
+	}, retryOpts...,
+	)
+
 	if err != nil {
 		b.log(ctx).Error(errors.Details(err))
 		return nil, errors.Trace(err)
